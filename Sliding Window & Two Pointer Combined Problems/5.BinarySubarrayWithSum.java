41621d7ed54c30d610041645b7338ec22b75f8f0
Tushar Tripathi <33855154+tushartripathi1998@users.noreply.github.com>
2025-07-07 01:22:48 +0530

    Create 5.BinarySubarrayWithSum.java

---
+++ Sliding Window & Two Pointer Combined Problems/5.BinarySubarrayWithSum.java
@@ -0,0 +1,26 @@
+// Binary subarray with sum
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] arr = {1, 0, 1, 0, 1};
+        int sum = 2;
+        int calculatedSum = 0;
+        
+        for (int i=0;i<arr.length;i++) {
+            calculatedSum = 0;
+            for (int j=i;j<arr.length;j++) {
+                calculatedSum = arr[j] + calculatedSum;
+                if (calculatedSum > sum) {
+                    break;
+                } else {
+                    if (calculatedSum == sum) {
+                        System.out.println("set-["+i+","+j+"]");
+                    }
+                }
+            }
+        }
+    }
+}
